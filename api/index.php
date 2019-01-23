c712dcbd0a04d02cb61afec0aec89d35b21bd98e
Yehuda Zadik <yehudaz@one1.co.il>
2019-01-23 10:36:48 +0200

    Adding task

--- api/index.php
+++ api/index.php
@@ -18,42 +18,42 @@ function getAllTasks($pdo) {
     return $result;
 }
 
-switch ($_GET['action']) {
-    case 'add':
-        $name = $_GET['name'];
-        $statement = $pdo->prepare('INSERT INTO tasks (name) VALUES (:name)');
+if ( isset($_GET['action']) && $_GET['action'] == "getAll" ) {
+    $result = getAllTasks($pdo);
+    echo json_encode($result);
+} else {
+    switch ($_POST['action']) {
+        case 'add':
+            $name = $_POST['name'];
+            $statement = $pdo->prepare('INSERT INTO tasks (name) VALUES (:name)');
 
-        $statement->execute(['name' => $name]);
+            $statement->execute(['name' => $name]);
 
-        $result = getAllTasks($pdo);
-        echo json_encode($result);
-        break;
+            $result = getAllTasks($pdo);
+            echo json_encode($result);
+            break;
 
-    case 'edit':
-        $id = $_GET['id'];
-        $name = $_GET['name'];
-        $is_completed = $_GET['is_completed'];
+        case 'edit':
+            $id = $_GET['id'];
+            $name = $_GET['name'];
+            $is_completed = $_GET['is_completed'];
 
-        $sql = "UPDATE users SET name=?, is_completed=? WHERE id=?";
-        $stmt= $pdo->prepare($sql);
-        $stmt->execute([$name, $is_completed, $id]);
+            $sql = "UPDATE users SET name=?, is_completed=? WHERE id=?";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$name, $is_completed, $id]);
 
-        $result = getAllTasks($pdo);
-        echo json_encode($result);
-        break;
+            $result = getAllTasks($pdo);
+            echo json_encode($result);
+            break;
 
-    case 'delete':
-        $id = $_GET['id'];
+        case 'delete':
+            $id = $_GET['id'];
 
-        $q = $pdo->prepare("DELETE FROM tasks WHERE id = ?");
-        $q->execute(array($id));
+            $q = $pdo->prepare("DELETE FROM tasks WHERE id = ?");
+            $q->execute(array($id));
 
-        $result = getAllTasks($pdo);
-        echo json_encode($result);
-        break;
-
-    case 'getAll':
-        $result = getAllTasks($pdo);
-        echo json_encode($result);
-        break;
+            $result = getAllTasks($pdo);
+            echo json_encode($result);
+            break;
+    }
 } ?>
--- js/todo.js
+++ js/todo.js
@@ -14,9 +14,15 @@ function arrangeTasksData() {
 
     tasks.forEach(function(task, i) {
         var row = '<tr> '
-            + '<td>' + (i+1) + '</td>'
-            + '<td>'+ task.name + '</td>'
-            + '<td><button onclick=showEditModal(' + task.id + ',"' + task.name + '")>Edit</button></td>'
+            + '<td>' + (i+1) + '</td>';
+
+        if ( task.is_completed == 1) {
+            row += '<td class="task-completed">' + task.name + '</td>';
+        } else {
+            row += '<td>' + task.name + '</td>';
+        }
+
+        row += '<td><button onclick=showEditModal(' + task.id + ',"' + task.name + '")>Edit</button></td>'
             + '</tr>';
         rows += row;
 
@@ -45,7 +51,7 @@ function getTasks() {
 function addTask(name) {
     $.ajax({
         url: "api/index.php",
-        method: "GET",
+        method: "post",
         data: { action : 'add', name: name }
     }).done(function( response ) {
         tasks = JSON.parse(response);
@@ -57,7 +63,7 @@ function editTask(id, name, isCompleted) {
 
     $.ajax({
         url: "api/index.php",
-        method: "GET",
+        method: "post",
         data: { action : 'edit', id: id, name: name, is_completed: isCompleted }
     }).done(function( response ) {
         tasks = JSON.parse(response);
@@ -68,7 +74,7 @@ function editTask(id, name, isCompleted) {
 function deleteTask(id) {
     $.ajax({
         url: "api/index.php",
-        method: "GET",
+        method: "post",
         data: { action : 'delete', id: id }
     }).done(function( response ) {
         tasks = JSON.parse(response);
@@ -78,14 +84,34 @@ function deleteTask(id) {
 
 function showEditModal(id, name){
     $('#bs-example-modal-sm').modal('show');
-    $('name').text(name)
+    $('#name').val(name);
+
+    $('#task-status').removeClass('hidden');
+
+    $("#save-button").attr('data-id', id);
 }
 
 function showAddModal() {
-    $('name').text('')
+    $('#bs-example-modal-sm').modal('show');
+
+    $('#name').val('');
+
+    $('#task-status').addClass('hidden');
+
+    $("#save-button").attr('data-id', -1);
 }
 
 
 $(document).ready(function () {
 
+
+    $('#save-button').click( function () {
+        var data_id = $(this).attr('data-id');
+
+        if ( data_id == -1 ) {
+            // Adding task
+            addTask( $('#name').val() );
+        } 
+        $('#bs-example-modal-sm').modal('hide');
+    });
 });
